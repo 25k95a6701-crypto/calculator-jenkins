7ac64eb1c47e5af799ff1b380a372aa64ae27907
25k95a6701-crypto <25k95a6701@tkrcet.com>
2026-09-24 16:20:52 +0530

    changes made

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,4 @@
-mport static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 public class CalculatorTest {
     Calculator calculator = new Calculator();
