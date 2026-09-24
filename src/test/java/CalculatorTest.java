bc8aa708b2cecca9afd486a8be64744a7d2e64f5
25k95a6701-crypto <25k95a6701@tkrcet.com>
2026-09-24 15:28:16 +0530

    Initial calculator project

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+mport static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+    @Test
+    public void testAddition() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+    @Test
+    public void testSubtraction() {
+        assertEquals(6, calculator.subtract(10, 4));
+    }
+    @Test
+    public void testMultiplication() {
+        assertEquals(20, calculator.multiply(5, 4));
+    }
+    @Test
+    public void testDivision() {
+        assertEquals(4, calculator.divide(20, 5));
+    }
+}
